849e8715c44e638394810bb8e6114d6e09c2ab83
Ella Crowder <ellawho4@uw.edu>
2024-09-10 17:43:18 -0700

    Initial Commit

---
+++ Capstone_IOS/Capstone_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Capstone_IOSApp.swift
+//  Capstone_IOS
+//
+//  Created by Ella Crowder on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Capstone_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
